0af9b8fb117cfe0e677f57b046980c5673147414
PTBYSR <paul.emechebe@gmail.com>
2024-06-19 21:33:43 +0100

    fix

--- app/contact/page.js
+++ app/contact/page.js
@@ -23,7 +23,7 @@ const MusicPlayer = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg">
         <h1 className="text-2xl font-bold mb-4">React Music Player</h1>
-        <audio ref={audioRef} src={audio[0].src} />
+        {/* <audio ref={audioRef} src={audio[0].src} /> */}
         <button
           onClick={togglePlayPause}
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
